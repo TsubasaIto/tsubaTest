e315bb3ba610d012e9786bf1d230b34639fee188
tsubasaito <ito.tsubasa@fixer.co.jp>
2015-06-22 16:58:30 +0900

    test

---
+++ TestApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A a = new A();
+
+            var b = a.Aprop;
+
+        }
+    }
+
+
+    public class A
+    {
+        public A()
+        {
+        }
+        public string Aprop { get; set; }
+    }
+}
